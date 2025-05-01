f37cf30733c9cee959d1be5b4d5fffc1e665595d
Rune Aagaard Keena <rune.aagaard.keena@randers.dk>
2025-05-01 10:25:51 +0200

    added admin login

--- src/main.py
+++ src/main.py
@@ -1,233 +1,72 @@
+import sys
+import io
+import logging
 import pandas as pd
-import streamlit as st
-from streamlit_keycloak import login
-from streamlit_tree_select import tree_select
 
+from datetime import datetime
+from streamlit.web import cli as stcli
 from sqlalchemy import text
+from apscheduler.schedulers.background import BackgroundScheduler
 
 from utils.database import DatabaseClient
+from utils.sftp import SFTPClient
 from utils.config import DB_HOST, DB_USER, DB_PASS, DB_NAME
-from utils.config import KEYCLOAK_URL, KEYCLOAK_REALM, KEYCLOAK_CLIENT_ID
+from utils.logging import set_logging_configuration
+
+# Set up logging configuration
+set_logging_configuration()
+
+logger = logging.getLogger(__name__)
 
 SCHEMA = "meddb"
+sched = BackgroundScheduler()
 db_client = DatabaseClient(db_type="postgresql", database=DB_NAME, username=DB_USER, password=DB_PASS, host=DB_HOST)
+sftp_client = SFTPClient(host="sftp.randers.dk", username="SFTP-SDRoller", password="rRg8Uy699QNzukvLBeP%ok6K3^v^sYSC")
 
 with db_client.get_connection() as conn:
     conn.execute(text(f"CREATE SCHEMA IF NOT EXISTS {SCHEMA}"))
     conn.commit()
 
 
-st.set_page_config(page_title="MED-Database", page_icon="🗄️", layout="wide")
-st.markdown('<style>table {width:100%;}</style>', unsafe_allow_html=True)
+def daily_job():
+    with sftp_client.get_connection() as sftp_conn:
+        file_list = sftp_conn.listdir()
+        csv_files = [file for file in file_list if file.endswith('.csv')]
 
+        if len(csv_files) == 0:
+            logger.warning("Warning: No CSV files found.")
+            return
+        elif len(csv_files) > 1:
+            logger.warning("Warning: More than one CSV file found.")
+            return
+        else:
+            csv_file_name = csv_files[0]
 
-# Fetch rows from the database only once
-if "udvalg_data" not in st.session_state:
-    with db_client.get_connection() as conn:
-        try:
-            query = 'SELECT id, overordnetudvalg, udvalg FROM meddb.udvalg'
-            result = conn.execute(text(query))
-            st.session_state.udvalg_data = result.mappings().all()  # Store rows as dictionaries in session state
-        except Exception as e:
-            st.error(f"Error fetching data from the database: {e}")
-            st.session_state.udvalg_data = None
-
-if "checked_nodes" not in st.session_state:
-    st.session_state.checked_nodes = []
-
-if "expanded_nodes" not in st.session_state:
-    st.session_state.expanded_nodes = []
-
-
-keycloak = login(
-    url=KEYCLOAK_URL,
-    realm=KEYCLOAK_REALM,
-    client_id=KEYCLOAK_CLIENT_ID
-)
-
-if keycloak.authenticated:
-    # TODO: Check if user is admin - has to be checked in SD Roles
-    email = keycloak.user_info['email']
-    st.write(f"Logget ind som: {keycloak.user_info['name']}")
-else:
-    email = None
-
-
-if email == 'rune.aagaard.keena@randers.dk':
-
-    uploaded_file = st.file_uploader("Upload file", type=["csv"], label_visibility="collapsed")
-    if uploaded_file is not None:
-        file_base_name = uploaded_file.name.split(".")
-        if len(file_base_name) == 2:
-            table_name = file_base_name[0]
-            table_name = table_name.lower()
-            with db_client.get_connection() as conn:
-
-                dataframe = pd.read_csv(uploaded_file, delimiter=";", encoding="utf-8", low_memory=False)
-
-                dataframe = dataframe.dropna(axis=1, how='all')
-
-                dataframe.columns = [col.lower() for col in dataframe.columns]
-
-                columns = []
-                for col_name, dtype in zip(dataframe.columns, dataframe.dtypes):
-                    if col_name == "id":
-                        columns.append(f"{col_name} INTEGER PRIMARY KEY")  # Define as INTEGER PRIMARY KEY
-                    elif col_name.endswith("Id"):
-                        columns.append(f"{col_name} INTEGER")  # Assuming foreign keys are integers
-                    elif pd.api.types.is_integer_dtype(dtype):
-                        columns.append(f"{col_name} INTEGER")
-                    elif pd.api.types.is_float_dtype(dtype):
-                        columns.append(f"{col_name} FLOAT")
-                    elif pd.api.types.is_bool_dtype(dtype):
-                        columns.append(f"{col_name} BOOLEAN")
-                    elif pd.api.types.is_datetime64_any_dtype(dtype):
-                        columns.append(f"{col_name} TIMESTAMP")
+            try:
+                with sftp_conn.open(csv_file_name, 'rb') as binary_file:
+                    decoded_file = io.TextIOWrapper(binary_file, encoding='UTF-16')
+                    df = pd.read_csv(decoded_file, sep=';')
+                    df.columns = df.columns.str.lower()
+                    if 'email' in df.columns:
+                        df = df[['email']]
+                        df['email'] = df['email'].str.lower()
+                        df = df.drop_duplicates(subset=['email'], ignore_index=True)
+                        with db_client.get_connection() as conn:
+                            df.to_sql('administratorer', conn, schema=SCHEMA, if_exists='replace', index=False)
+                            conn.commit()
+                            logger.info("Admins have been updated in database")
                     else:
-                        columns.append(f"{col_name} TEXT")
+                        logger.error("Error: 'email' column not found in the CSV file.")
+                        return
 
-                create_table_query = f"""
-                CREATE TABLE IF NOT EXISTS {SCHEMA}.{table_name} (
-                    {', '.join(columns)}
-                )
-                """
-
-                conn.execute(text(create_table_query))
-                conn.commit()
-
-                # Add foreign key constraints for columns ending with "Id"
-                for col_name in dataframe.columns:
-                    if col_name.endswith("Id") and col_name != "id":
-                        referenced_table = col_name[:-2].lower()  # Remove "Id" to get the referenced table name
-                        if referenced_table == "fag":
-                            referenced_table = "fagforening"
-                        add_foreign_key_query = f"""
-                        ALTER TABLE {SCHEMA}.{table_name}
-                        ADD CONSTRAINT fk_{table_name}_{col_name}
-                        FOREIGN KEY ({col_name})
-                        REFERENCES {SCHEMA}.{referenced_table}(Id)
-                        ON DELETE CASCADE
-                        """
-                        conn.execute(text(add_foreign_key_query))
-                        conn.commit()
-
-                # Insert the data into the table
-                dataframe.to_sql(table_name, conn, if_exists="append", index=False, schema=SCHEMA)
-
-                st.success(f"The table '{table_name}' has been created successfully.")
-                conn.commit()
-        else:
-            st.error("File name is not valid. Please use a file name with only one dot.")
-            result = conn.execute(text("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"))
-            tables = [row['table_name'] for row in result]
-            st.write("Tables in the database:", tables)
+            except Exception as e:
+                logger.error(f"Error reading CSV file: {e}")
+                return
 
 
-# Use the cached data to build the tree structure
-rows = st.session_state.udvalg_data
-
-if rows:
-
-    # Convert rows into a dictionary for tree structure
-    nodes = []
-    row_dict = {row['id']: {'label': row['udvalg'], 'value': row['id']} for row in rows}
-
-    # Build the tree structure
-    for row in rows:
-        parent_id = row['overordnetudvalg']
-        if parent_id is None and row['udvalg'] == 'HOVEDUDVALG':  # Not right - but some 'deleted'? udvalg are still there with no parent
-            # Top-level node
-            nodes.append(row_dict[row['id']])
-        else:
-            # Add as a child to its parent
-            if parent_id in row_dict:
-                if 'children' not in row_dict[parent_id]:
-                    row_dict[parent_id]['children'] = []
-                row_dict[parent_id]['children'].append(row_dict[row['id']])
-
-    # Create two columns for layout
-    col1, col2 = st.columns([1, 2])
-
-    # Display the tree in the left column
-    with col1:
-        # Rerender the tree with updated checked nodes
-        selected = tree_select(
-            nodes,
-            no_cascade=True,
-            expanded=st.session_state.expanded_nodes,  # Default expanded nodes
-            checked=st.session_state.checked_nodes  # Use session state for checked nodes
-        )
-
-        if selected:
-            new_checked_nodes = selected.get("checked", [])
-            if len(new_checked_nodes) == 2:
-                # Take the value not in the session state
-                new_checked_nodes = [node for node in new_checked_nodes if node not in st.session_state.checked_nodes]
-
-            if len(new_checked_nodes) == 1:
-                expanded_nodes = []
-                current_node = int(new_checked_nodes[0])
-                while current_node is not None:
-                    expanded_nodes.append(current_node)
-                    parent_node = next((row['overordnetudvalg'] for row in rows if row['id'] == current_node), None)
-                    current_node = parent_node
-
-                st.session_state.expanded_nodes = expanded_nodes
-
-            # If a new node is selected, update the session state
-            if len(new_checked_nodes) == 1 and new_checked_nodes != st.session_state.checked_nodes:
-                st.session_state.checked_nodes = new_checked_nodes  # Update to the new selection
-                st.rerun()  # Rerun the app to reflect the new selection
-
-            # If two nodes are selected, keep only the newest one
-            if len(new_checked_nodes) == 2:
-                st.session_state.checked_nodes = [new_checked_nodes[-1]]  # Keep the last selected node
-                st.rerun()
-
-            # If more than two nodes are selected, show a warning and reset
-            if len(new_checked_nodes) > 2:
-                st.warning("Multiple nodes selected. Resetting selection...")
-                st.session_state.checked_nodes = []  # Clear the checked nodes
-                st.rerun()
-
-    # Display the selected 'udvalg' in the right column
-    with col2:
-        if st.session_state.checked_nodes:
-            # Ensure only one node is selected
-            item = st.session_state.checked_nodes[0]
-            selected_node = row_dict.get(int(item), {})
-            if selected_node:
-                st.header(selected_node.get('label', 'Unknown'))
-                with db_client.get_connection() as conn:
-                    query = f"""
-                    SELECT p.navn, r.titelkursus as rolle, p.email
-                    FROM {SCHEMA}.personrolle pr
-                    JOIN {SCHEMA}.rolle r ON pr.rolleid = r.id
-                    JOIN {SCHEMA}.person p ON pr.personid = p.id
-                    WHERE pr.udvalgsid = :udvalgsid
-                    """
-                    result = conn.execute(text(query), {"udvalgsid": selected_node['value']})
-                    roles_and_persons = result.mappings().all()
-
-                # Display the result in a DataFrame
-                df = pd.DataFrame(roles_and_persons)
-                if not df.empty:
-                    df = df[['navn', 'rolle', 'email']]
-                    # Capitalize column names
-                    df.columns = [col.capitalize() for col in df.columns]
-
-                    # Sort the dataframe to prioritize specific roles
-                    priority_roles = ['Formand', 'Næstformand', 'Sekretær', 'Udvalgsadministrator']
-                    df['Priority'] = df['Rolle'].apply(lambda x: priority_roles.index(x) if x in priority_roles else len(priority_roles))
-                    df = df.sort_values(by=['Priority', 'Rolle', 'Navn']).drop(columns=['Priority'])
-
-                    # Convert email values to hyperlinks
-                    df['Email'] = df['Email'].apply(lambda x: f'<a href="mailto:{x}">{x}</a>')
-
-                    st.write(df.style.hide(axis="index").set_properties(**{'text-align': 'left'}).to_html(escape=False), unsafe_allow_html=True)
-                else:
-                    st.write("Ingen personer fundet på det valgte udvalg.")
-            else:
-                st.error("Selected node not found.")
-        else:
-            st.write("No udvalg selected.")
+if __name__ == "__main__":  # pragma: no cover
+    sched.add_job(daily_job, 'date', run_date=datetime.now())
+    sched.add_job(daily_job, 'cron', hour=8, minute=0, id='daily_job')
+    sched.start()
+    sys.argv = ["streamlit", "run", "streamlit_app.py", "--client.toolbarMode=minimal", "--server.port=8080"]
+    sys.exit(stcli.main())
